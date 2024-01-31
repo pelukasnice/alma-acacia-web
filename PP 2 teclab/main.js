3b2145acb30eaa65dbdf70fb4725bd5f00964f4e
pelukasnice <satakanaska@gmail.com>
2024-01-31 11:47:10 -0300

    scroll js

--- PP 2 teclab/main.js
+++ PP 2 teclab/main.js
@@ -10,7 +10,28 @@ document.addEventListener("click", function(e) {
     }
 })
 
+/* SCROLL NAVBAR */
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona todos los enlaces del navbar
+    var links = document.querySelectorAll('nav a');
 
+    // Agrega un evento de clic a cada enlace
+    links.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            // Previene el comportamiento predeterminado del enlace
+            event.preventDefault();
+
+            // Obtiene el objetivo del enlace (el atributo href)
+            var targetId = this.getAttribute('href');
+
+            // Desplazamiento suave hacia el objetivo
+            document.querySelector(targetId).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });
+});
 
 
 
